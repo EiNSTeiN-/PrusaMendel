b594eada40611d1ea8558ab84745df3118e78056
EiNSTeiN_ <einstein@einstein-laptop-2.(none)>
2012-09-09 18:53:24 -0400

    recompiled some parts

--- generate-metric-prusa-lm8uu.sh
+++ generate-metric-prusa-lm8uu.sh
@@ -7,9 +7,9 @@ openscad -s metric-prusa-lm8uu/frame-vertex-neo-foot.stl -D 'basefoot=true;' sou
 openscad -s metric-prusa-lm8uu/pulley-GT2-20T.stl -D 'teeth=20;profile=12;motor_shaft=5.2;retainer=1;' source/Pulley_T-MXL-XL-HTD-GT2_N-tooth.scad
 openscad -s metric-prusa-lm8uu/gregs-herringbone-small.stl -D 'small=true;' source/Wades_Gears_helix.scad
 openscad -s metric-prusa-lm8uu/gregs-herringbone-big.stl -D 'small=false;' source/Wades_Gears_helix.scad
-openscad -s metric-prusa-lm8uu/gregs-jhead_mount -D 'hotend_mount=jhead_mount;' source/jonaskuehling_gregs-wade-v3.scad
+openscad -s metric-prusa-lm8uu/gregs-jhead_mount.stl -D 'hotend_mount=jhead_mount;' source/jonaskuehling_gregs-wade-v3.scad
 openscad -s metric-prusa-lm8uu/ybrac-t.stl source/ybrac-t.scad
-openscad -s metric-prusa-lm8uu/z-motor-mount.stl source/integrated-z-motor-mount.scad
+openscad -s metric-prusa-lm8uu/z-motor-mount-barclamp.stl source/z-motor-mount-barclamp.scad
 openscad -s metric-prusa-lm8uu/x-end-idler.stl -D 'linear=true;lme8uu=false' source/x-end-idler.scad
 openscad -s metric-prusa-lm8uu/x-end-motor.stl -D 'linear=true;lme8uu=false' source/x-end-motor.scad
 openscad -s metric-prusa-lm8uu/x-carriage-doublefan.stl source/llluis-gregs-new-x-carriage-double.scad
